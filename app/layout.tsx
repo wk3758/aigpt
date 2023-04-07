30a8da8c609061aa4e6af14226fc01d5777225a5
wk3758 <wk3758@163.com>
2023-04-07 22:34:58 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "AIGPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "AIGPT",
     statusBarStyle: "black-translucent",
   },
   themeColor: "#fafafa",
