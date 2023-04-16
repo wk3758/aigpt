705cb36eec2481eed1d537504df67e2f81502152
wk3758 <wk3758@163.com>
2023-04-16 21:50:08 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -630,9 +630,9 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>AIGPT智能助手</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            AIGPT：你最佳的人工智能助手.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
