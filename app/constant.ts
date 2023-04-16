16a50fedecb0c567ee934ab01fcc575f5cedab3e
wk3758 <wk3758@163.com>
2023-04-16 21:58:22 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,7 @@
 export const OWNER = "wk3758";
 export const REPO = "AIGPT";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+//把我的github地址注释掉，不显示了；或者换一个链接
+// export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
 export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
